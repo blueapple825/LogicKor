faa9e3b9371b78bfbc0c2e38e474d1fa2a23208e
blueapple825 <blueapple8259@gmail.com>
2025-11-27 00:41:17 +0900

    llama.cpp로 코드 변경

--- generator.py
+++ generator.py
@@ -2,19 +2,9 @@ import argparse
 import os
 
 import pandas as pd
+from llama_cpp import Llama
 
 from templates import PROMPT_STRATEGY
-
-# Use aphrodite-engine or vLLM
-try:
-    from aphrodite import LLM, SamplingParams
-
-    print("- Using aphrodite-engine")
-
-except ImportError:
-    from vllm import LLM, SamplingParams
-
-    print("- Using vLLM")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-g", "--gpu_devices", help=" : CUDA_VISIBLE_DEVICES", default="0")
@@ -32,20 +22,16 @@ print(f"Args - {args}")
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_devices
 gpu_counts = len(args.gpu_devices.split(","))
 
-llm = LLM(
-    model=args.model,
-    tensor_parallel_size=gpu_counts,
-    max_model_len=args.model_len,
-    gpu_memory_utilization=0.8,
-    trust_remote_code=True,  # !
+llm = Llama(
+    model_path=args.model,
+    n_gpu_layers=-1,
+    n_ctx=args.model_len,
+    verbose=False,
 )
 
-sampling_params = SamplingParams(
-    temperature=0,
-    skip_special_tokens=True,
-    max_tokens=args.model_len,
-    stop=["<|endoftext|>", "[INST]", "[/INST]", "<|im_end|>", "<|end|>", "<|eot_id|>", "<end_of_turn>", "<eos>"],
-)
+temperature = 0
+max_tokens = args.model_len
+stop = ["<|endoftext|>", "[INST]", "[/INST]", "<|im_end|>", "<|end|>", "<|eot_id|>", "<end_of_turn>", "<eos>"]
 
 df_questions = pd.read_json("questions.jsonl", orient="records", encoding="utf-8-sig", lines=True)
 
@@ -54,38 +40,35 @@ if not os.path.exists("./generated/" + args.model):
 
 for strategy_name, prompts in PROMPT_STRATEGY.items():
 
-    def format_single_turn_question(question):
-        return llm.llm_engine.tokenizer.tokenizer.apply_chat_template(
-            prompts + [{"role": "user", "content": question[0]}],
-            tokenize=False,
-            add_generation_prompt=True,
+    def single_turn_question(question):
+        output = llm.create_chat_completion(
+            messages=prompts + [{"role": "user", "content": question[0]}],
+            temperature=temperature,
+            stop=stop,
+            max_tokens=max_tokens,
         )
+        return output["choices"][0]["message"]["content"].strip()
 
-    single_turn_questions = df_questions["questions"].map(format_single_turn_question)
-    print(single_turn_questions.iloc[0])
-    single_turn_outputs = [
-        output.outputs[0].text.strip() for output in llm.generate(single_turn_questions, sampling_params)
-    ]
+    single_turn_outputs = df_questions["questions"].map(single_turn_question)
 
-    def format_double_turn_question(question, single_turn_output):
-        return llm.llm_engine.tokenizer.tokenizer.apply_chat_template(
-            prompts
+    def double_turn_question(question, single_turn_output):
+        output = llm.create_chat_completion(
+            messages=prompts
             + [
                 {"role": "user", "content": question[0]},
                 {"role": "assistant", "content": single_turn_output},
                 {"role": "user", "content": question[1]},
             ],
-            tokenize=False,
-            add_generation_prompt=True,
+            temperature=temperature,
+            stop=stop,
+            max_tokens=max_tokens,
         )
+        return output["choices"][0]["message"]["content"].strip()
 
-    multi_turn_questions = df_questions[["questions", "id"]].apply(
-        lambda x: format_double_turn_question(x["questions"], single_turn_outputs[x["id"] - 1]),
+    multi_turn_outputs = df_questions[["questions", "id"]].apply(
+        lambda x: double_turn_question(x["questions"], single_turn_outputs[x["id"] - 1]),
         axis=1,
     )
-    multi_turn_outputs = [
-        output.outputs[0].text.strip() for output in llm.generate(multi_turn_questions, sampling_params)
-    ]
 
     df_output = pd.DataFrame(
         {
